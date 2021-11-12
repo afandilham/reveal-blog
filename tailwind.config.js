0736855b150f3aee402e4f3277d13031ddcbf155
Moch. Ilham Afandi <afandilham@gmail.com>
2021-11-12 17:39:43 +0700

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      'poppins': "'Poppins', sans-serif",
+      'fira': "'Fira Mono', mono"
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
